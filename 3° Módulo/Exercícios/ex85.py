722a33ce512ff108ef46b9a7811eda0c872270d9
João Vitor <olhierturcojoaovitor@gmail.com>
2026-06-22 15:08:22 -0300

    Create ex85.py

---
+++ 3° Módulo/Exercícios/ex85.py
@@ -0,0 +1,28 @@
+valores = [[], []]
+
+for c in range(3):
+    n = int(input('Digite um valor: '))
+
+    if n % 2 == 0:
+        valores[0].append(n)
+    else:
+        valores[1].append(n)
+
+valores[0].sort()
+valores[1].sort()
+
+print('-'*30)
+
+if valores[0]:
+    print(f'Os valores pares são (em ordem crescente):')
+    for valor in valores[0]:
+        print(valor)
+else:
+    print('Não há valores pares')
+
+if valores[1]:
+    print('Os valores impares são (em ordem crescente):')
+    for valor in valores[1]:
+        print(f'{valor}')
+else:
+    print('Não há valores impares')
